5396e209411247da3d4d02264815ab694da0c60c
Paul Christian <paulchristianmasayon2020@gmail.com>
2023-09-11 03:12:59 +0800

    placeholder posters & Movie folder moved

--- src/components/MainSections/SearchResults/Movie/Movie.js
+++ src/components/MainSections/SearchResults/Movie/Movie.js
@@ -1,3 +1,4 @@
+import placeholderPoster from "../../assets/poster-placeholder.png";
 export default function Movie({ title, year, poster, selectMovieHandler, id }) {
   return (
     <li
@@ -10,7 +11,7 @@ export default function Movie({ title, year, poster, selectMovieHandler, id }) {
     >
       <div>
         <img
-          src={poster}
+          src={poster === "N/A" ? placeholderPoster : poster}
           alt="movie poster"
           style={{ width: "65px", height: "70px" }}
         />
